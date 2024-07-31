b7962900266a9f9015106878ab811cdbaba26bfc
zhengxyz123 <3422794023@qq.com>
2024-07-31 17:53:35 +0800

    add function and variable support.

--- zcalc.py
+++ zcalc.py
@@ -25,11 +25,19 @@ import argparse
 import math
 import re
 import sys
+from fractions import Fraction
 from typing import Iterator, NamedTuple
 
+try:
+    import readline
+    import rlcompleter
+
+    is_rl_available = True
+except ModuleNotFoundError:
+    is_rl_available = False
 
 __version__ = "0.9"
-keywords = ["exit", "get", "set", "solve", "diff", "int"]
+keywords = ["exit", "get", "set", "solve", "sum", "diff", "int"]
 
 
 class ZCalcError(Exception):
@@ -40,15 +48,6 @@ class ZCalcError(Exception):
         self.position = position
         self.message = message or f"found an error at column {position[0] + 1}"
         super().__init__(self.message)
-
-
-def display_error(error: ZCalcError) -> None:
-    print(f"{error.message}:")
-    print(f"  {error.code}")
-    highlight = " " * error.position[0] + "^"
-    if error.position[1] - error.position[0] > 1:
-        highlight += "^" * (error.position[1] - error.position[0] - 1)
-    print(f"  {highlight}")
 
 
 class Token(NamedTuple):
@@ -62,6 +61,15 @@ class Statement(NamedTuple):
     code: str
     expr: list[Token]
     aftersep: list[Token] | None
+
+
+def display_error(error: ZCalcError) -> None:
+    print(f"{error.message}:")
+    print(f"  {error.code}")
+    highlight = " " * error.position[0] + "^"
+    if error.position[1] - error.position[0] > 1:
+        highlight += "^" * (error.position[1] - error.position[0] - 1)
+    print(f"  {highlight}")
 
 
 class Context:
@@ -110,12 +118,17 @@ class Context:
         }
         if sys.version_info >= (3, 11):
             self._functions.setdefault("cbrt", (math.cbrt, 1))
+        self._settings: dict[str, int] = {
+            "enable_num2str": 1,
+            "num2str_max_num1": 1000,
+            "num2str_max_num2": 1000,
+        }
         self._variables: dict[str, int | float] = {
             "e": math.e,
-            "phi": (math.sqrt(5) - 1) / 2,
             "pi": math.pi,
             "tau": math.tau,
         }
+        self.redirected_stdin = False
 
     def _shunting_yard(self, tokens: list[Token]) -> list[Token]:
         def _get_priority(op: str) -> int:
@@ -128,7 +141,11 @@ class Context:
         for token in tokens:
             if token.type == "keyword":
                 raise ZCalcError(self._code, token.where)
-            if token.type == "num":
+            elif token.type == "equal":
+                raise ZCalcError(
+                    self._code, token.where, '"=" can\'t be used in expression'
+                )
+            elif token.type == "num":
                 output.append(token)
             elif token.type == "name":
                 if token.value in self._functions:
@@ -162,6 +179,135 @@ class Context:
                 raise ZCalcError(self._code, op.where, 'missing ")"')
             output.append(op)
         return output
+
+    def _simplify(self, value: int) -> tuple[int, int]:
+        flag = 1 if value > 0 else -1
+        value = abs(value)
+        if value == 1:
+            return (flag, 1)
+        i, cache, inner, outer = 2, [], value, 1
+        while inner != 1:
+            for m in [i, i - 1]:
+                if cache.count(m) == 2:
+                    outer *= m
+                    cache.remove(m)
+                    cache.remove(m)
+            if math.gcd(inner, i) != 1:
+                inner //= i
+                cache.append(i)
+            else:
+                i += 1
+        if cache.count(i) == 2:
+            outer *= i
+            cache.remove(i)
+            cache.remove(i)
+        return flag * outer, math.prod(cache)
+
+    def _num2sqrts(self, n: float) -> tuple[int, int] | None:
+        if n >= 0:
+            mid = math.floor((n / 2) ** 2) + 0.5
+        else:
+            mid = math.ceil(-((n / 2) ** 2)) - 0.5
+
+        def fsqrt(n: float) -> float:
+            return math.copysign(math.sqrt(math.fabs(n)), n)
+
+        actual_mid = n / 2
+        t = 0.5
+        while True:
+            a = fsqrt(mid + t)
+            d = math.fabs(a - actual_mid)
+            b = actual_mid - d
+            b = fsqrt(math.copysign(round(b**2), b))
+            if (
+                abs(a**2) > self._settings["num2str_max_num1"]
+                or abs(b**2) > self._settings["num2str_max_num1"]
+            ):
+                return
+            if math.isclose(a + b, n, rel_tol=1e-15):
+                return int(round(math.copysign(a**2, a))), int(
+                    round(math.copysign(b**2, b))
+                )
+            t += 1
+
+    def _num2str(
+        self,
+        value: int | float,
+        twice: bool = False,
+    ) -> str | None:
+        if self._settings["enable_num2str"] == 0:
+            return str(value)
+        if int(value) == value:
+            return str(int(value))
+        flag = "" if value > 0 else "-"
+        a, b = (
+            Fraction(value)
+            .limit_denominator(self._settings["num2str_max_num2"])
+            .as_integer_ratio()
+        )
+        if math.isclose(value, a / b):
+            return f"{a}/{b}"
+        a, b = (
+            Fraction(value**2)
+            .limit_denominator(self._settings["num2str_max_num2"])
+            .as_integer_ratio()
+        )
+        if math.isclose(value**2, a / b):
+            if b == 1:
+                outer, inner = self._simplify(a)
+                return f"{flag}{'' if outer == 1 else outer}sqrt({inner})"
+            elif (a == 1) and (b != 1):
+                return f"{flag}sqrt({b})/{b}"
+            else:
+                outer, inner = self._simplify(a * b)
+                fact = math.gcd(outer, b)
+                a, b = int(outer / fact), int(b / fact)
+                return f"{flag}{'' if a == 1 else a}sqrt({inner})/{b}"
+        if twice:
+            return
+        if (s := self._num2str(value / math.pi, twice=True)) is not None:
+            pi = chr(960)
+            if s.startswith("-1/") or s.startswith("1/"):
+                return s.replace("1/", pi + "/")
+            elif "/" in s:
+                return s.replace("/", pi + "/")
+            else:
+                if s in ["1", "-1"]:
+                    s = flag
+                return s + pi
+        for c in range(1, 101):
+            if (l := self._num2sqrts(value * c)) is not None:
+                outer_a, inner_a = self._simplify(l[0])
+                outer_b, inner_b = self._simplify(l[1])
+                flag = ""
+                if c != 1 and outer_a < 0 and outer_b < 0:
+                    flag = "-"
+                    outer_a, outer_b = -outer_a, -outer_b
+                if (inner_b == 1 and inner_a != 1) or (
+                    inner_a < inner_b and not abs(l[0]) > abs(l[1])
+                ):
+                    outer_a, outer_b = outer_b, outer_a
+                    inner_a, inner_b = inner_b, inner_a
+                if inner_a != 1:
+                    part_a = f"{'' if outer_a > 0 else '-'}"
+                    part_a += (
+                        f"{'' if abs(outer_a) == 1 else abs(outer_a)}sqrt({inner_a})"
+                    )
+                else:
+                    part_a = str(outer_a)
+                if inner_b != 1:
+                    part_b = f"{'+' if outer_b > 0 else '-'}"
+                    part_b += (
+                        f"{'' if abs(outer_b) == 1 else abs(outer_b)}sqrt({inner_b})"
+                    )
+                else:
+                    part_b = ("+" if outer_b > 0 else "") + str(outer_b)
+                expr = part_a + part_b
+                if c == 1:
+                    return expr
+                else:
+                    return f"{flag}({expr})/{c}"
+        return str(value)
 
     def _is_exit_stmt(self, tokens: list[Token]) -> bool:
         if not (tokens[0].type == "keyword" and tokens[0].value == "exit"):
@@ -202,9 +348,10 @@ class Context:
             raise ZCalcError(self._code, tokens[1].where)
         return True
 
-    def _is_sdi_stmt(self, tokens: list[Token]) -> bool:
+    def _is_ssdi_stmt(self, tokens: list[Token]) -> bool:
         if not (
-            tokens[0].type == "keyword" and tokens[0].value in ["solve", "diff", "int"]
+            tokens[0].type == "keyword"
+            and tokens[0].value in ["solve", "sum", "diff", "int"]
         ):
             return False
         count_sep = 0
@@ -228,11 +375,11 @@ class Context:
             return False
         if not tokens[1].type == "equal":
             return False
-        if tokens[0].value in ["e", "phi", "pi", "tau"]:
+        if tokens[0].value in ["e", "pi", "tau"]:
             raise ZCalcError(self._code, tokens[0].where, "can't assign const variable")
         return True
 
-    def _parse_sdi_stmt(self, tokens: list[Token]) -> Statement:
+    def _parse_ssdi_stmt(self, tokens: list[Token]) -> Statement:
         sep_pos = 0
         for token in tokens:
             if token.type == "sep":
@@ -325,25 +472,23 @@ class Context:
                 raise ZCalcError(code, where, f'unknown symbol "{value}"')
             yield Token(kind, value, where)
 
-    def parse(self, code: str) -> Statement:
-        self._code = code
-        tokens = list(self.tokenize(self._code))
+    def parse(self, tokens: list[Token]) -> Statement:
         if self._is_exit_stmt(tokens):
             return Statement("exit", self._code, [], None)
         elif self._is_set_stmt(tokens):
             return Statement("set", self._code, tokens[1:], None)
         elif self._is_get_stmt(tokens):
             return Statement("get", self._code, tokens[1:], None)
-        elif self._is_sdi_stmt(tokens):
-            return self._parse_sdi_stmt(tokens)
+        elif self._is_ssdi_stmt(tokens):
+            return self._parse_ssdi_stmt(tokens)
         elif self._is_assignment_stmt(tokens):
             return Statement("assign", self._code, tokens, None)
         else:
             return Statement("expr", self._code, tokens, None)
 
-    def execute(self, stmt: Statement) -> int | float:
+    def calculate(self, exprs: list[Token]) -> int | float:
         stack = []
-        for token in self._shunting_yard(stmt.expr):
+        for token in self._shunting_yard(exprs):
             if token.value == "+":
                 stack.append(stack.pop() + stack.pop())
             elif token.value == "-":
@@ -369,9 +514,91 @@ class Context:
             elif token.value == "**":
                 a, b = stack.pop(), stack.pop()
                 stack.append(b**a)
-            else:
+            elif token.type == "name":
+                if token.value in self._variables:
+                    stack.append(self._variables[token.value])
+                elif token.value in self._functions:
+                    func, nargs = self._functions[token.value]
+                    if nargs > len(stack):
+                        raise ZCalcError(self._code, token.where)
+                    else:
+                        args = stack[-nargs:]
+                        stack.append(func(*args))
+                else:
+                    raise ZCalcError(
+                        self._code, token.where, f'name "{token.value}" is not defined'
+                    )
+            elif token.type == "num":
                 stack.append(token.value)
+            else:
+                raise ZCalcError(self._code, token.where)
+        if not isinstance(stack[-1], (int, float)) or len(stack) > 1:
+            orig_tokens = sorted(exprs, key=lambda expr: expr.where[0])
+            raise ZCalcError(
+                self._code,
+                (orig_tokens[0].where[0], orig_tokens[-1].where[1]),
+                "invalid expression",
+            )
         return stack[-1]
+
+    def get_setting(self, stmt: Statement) -> None:
+        name = stmt.expr[0].value
+        if name in self._settings and self.redirected_stdin:
+            print(f"{name}={self._settings[name]}")
+        else:
+            raise ZCalcError(
+                self._code, stmt.expr[0].where, f'setting name "{name}" is not defined'
+            )
+
+    def set_setting(self, stmt: Statement) -> None:
+        name = stmt.expr[0].value
+        if name in self._settings:
+            self._settings[name] = int(stmt.expr[2].value)
+            if not self.redirected_stdin:
+                print(f"{name}={self._settings[name]}")
+        else:
+            raise ZCalcError(
+                self._code, stmt.expr[0].where, f'setting name "{name}" is not defined'
+            )
+
+    def assign(self, stmt: Statement) -> None:
+        name = str(stmt.expr[0].value)
+        expr = stmt.expr[2:]
+        try:
+            result = self.calculate(self._shunting_yard(expr))
+        except IndexError:
+            raise ZCalcError(
+                self._code, (expr[0].where[0], expr[-1].where[1]), "invalid expression"
+            )
+        self._variables[name] = result
+        if name in self._functions:
+            raise ZCalcError(
+                self._code, stmt.expr[0].where, "can't assign a name of function"
+            )
+        if not self.redirected_stdin:
+            print(f"{name}={self._variables[name]}")
+
+    def execute(self, code: str) -> None:
+        self._code = code
+        tokens = list(self.tokenize(code))
+        stmt = self.parse(tokens)
+        if stmt.type == "exit":
+            sys.exit(0)
+        if stmt.type == "get":
+            self.get_setting(stmt)
+        elif stmt.type == "set":
+            self.set_setting(stmt)
+        elif stmt.type == "assign":
+            self.assign(stmt)
+        elif stmt.type == "expr":
+            try:
+                print(self._num2str(self.calculate(stmt.expr)))
+            except IndexError:
+                raise ZCalcError(
+                    self._code,
+                    (stmt.expr[0].where[0], stmt.expr[-1].where[1]),
+                    "invalid expression",
+                )
 
 
 def main() -> int:
@@ -386,20 +613,30 @@ def main() -> int:
 
     ctx = Context()
     if not sys.stdin.isatty():
+        ctx.redirected_stdin = True
         exprs = (s.strip() for s in sys.stdin.readlines())
         for expr in exprs:
             try:
                 if len(expr) > 0:
-                    if (expr := ctx.parse(expr)).type == "expr":
-                        print(ctx.execute(expr))
+                    ctx.execute(expr)
             except ZCalcError as error:
                 display_error(error)
+                return 1
         return 0
     if args.quiet:
         print(f"zcalc {__version__}, a simple calculator")
         print("Copyright (c) 2024 zhengxyz123")
         print("This is an open source software released under MIT license.")
-    return 0
+    while True:
+        try:
+            expr = input(">>> ")
+        except (EOFError, KeyboardInterrupt):
+            print()
+            continue
+        try:
+            ctx.execute(expr)
+        except ZCalcError as error:
+            display_error(error)
 
 
 if __name__ == "__main__":
